54679beae765725e051eec54a4ba0a01e165ad57
MR-SAIRAM-7 <mr.sairamofficial@gmail.com>
2024-02-18 15:48:18 +0530

    strings in js

---
+++ 34 stings in js/script.js
@@ -0,0 +1,26 @@
+// Strings in js
+
+// strings in js are like digital pieces of text that you can use to display words , sentences , or any kind of textual information
+
+// creating strings :- you can create a string by enclosing your text in single quotes ('')  or double quotes(" ")
+
+let str = "My name is SaiRam";
+console.log(str);
+
+
+// Combining two strings :- you can combine strings using the '+' operator.
+
+let a = "Sai";
+let b = "Ram";
+let c = a+b;
+console.log(c);
+
+// to get space between two strings
+
+let d = a +' '+b;
+console.log(d);
+
+// \n is new line character used to get output in newline
+
+// strings are used to display text on the screen , interact with users or perform operations involving text
+
