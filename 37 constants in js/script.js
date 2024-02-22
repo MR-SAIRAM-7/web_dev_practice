1899600cd5f61eb84aa00a152aad23afd16b634c
MR-SAIRAM-7 <mr.sairamofficial@gmail.com>
2024-02-22 22:00:30 +0530

    constant in js

---
+++ 37 constants in js/script.js
@@ -0,0 +1,8 @@
+// Constant in js 
+// in js 'const' is a keyword in to declare a variable whose value cannot be changed after it is assigned .
+
+const pi = 3.14;
+console.log(pi); // output is 3.14
+
+pi = 3.15;
+console.log(pi); // error TypeError: Assignment to constant variable. you cannot change constant once it is assigned
