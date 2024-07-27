4727b4479063304e033e003819638c6bd425e6c6
mr-sairam-7 <mr.sairamofficial@gmail.com>
2024-07-27 19:45:09 +0530

    interaction in js

---
+++ 42 interaction,alert,prompt,confirm in js/index.js
@@ -0,0 +1,22 @@
+// alert is used to simply display a message to the user
+
+let alrt = alert("You are in danger")
+console.log(alrt)
+
+//prompt is used to take user input
+
+// let name1 = prompt("Enter your name:");
+// if (name1 !== null && name1 !== "") {
+//     console.log("Name entered:", name1);
+// } else {
+//     console.log("No name entered.");
+// }
+
+
+//confirm is a built in function that displays a popup dialog box with two options ok and cancel 
+
+let delete_post = confirm("Do you really want to delete this post : ")
+console.log(delete_post)
+
+//if ok ---> returns true
+// if cancel ----> returns false
