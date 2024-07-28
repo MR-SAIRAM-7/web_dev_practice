670cc5ab7d388be9c109a65cc49caa4f44b36cc1
mr-sairam-7 <mr.sairamofficial@gmail.com>
2024-07-28 20:14:49 +0530

    insert adjacenthtml

---
+++ 53 insert adjacent html ,element text/script.js
@@ -0,0 +1,7 @@
+let bef = document.querySelector('.first')
+bef.insertAdjacentHTML('beforebegin','<h1>Iam before begin</h1>')
+bef.insertAdjacentHTML('afterbegin','<h1>Iam after begin</h1>')
+bef.insertAdjacentHTML('beforeend','<h1>Iam before end</h1>')
+bef.insertAdjacentHTML('afterend','<h1>Iam after end</h1>')
+
+
